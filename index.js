ed513db4019b7afbdd3223ee3f840a05fd28d128
cristina <cristinaxavierprodutora.gmail.com>
2021-07-02 01:51:48 -0300

    jsexercicio

---
+++ index.js
@@ -0,0 +1,52 @@
+
+let idade= "18"
+let souHumano = true
+
+
+if(idade >="18"){
+console.log('acesso concedido')
+}
+else{
+console.log('não concedido')
+}
+
+
+if((idade >= "18") && (souHumano == true)){
+console.log('ser for verdadeiras!')
+}
+
+else{
+    console.log('não verdadeiras')
+    }
+
+
+let jan= 'Janeiro'
+let dez= 'Dezembro'
+
+if((jan === 'janeiro' || dez === 'dezembro'))
+{
+console.log("Felicidades")
+}
+
+
+let nome= "Cristina"
+
+
+if(nome.substring(0,1) ==="R")
+{
+console.log('Seu nome começa com a "letra R"')
+}
+else {
+console.log('Seu nome não começa com a "letra R"')
+}
+
+
+let sobrenome= "Xavier"
+
+if(sobrenome.length >= '6 letras') 
+{
+console.log('Seu sobrenome tem mais de 6 letras')
+}
+else {
+console.log('seu nome não começa com a letra E')
+}
